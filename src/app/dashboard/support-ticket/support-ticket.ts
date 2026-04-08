91a9aad96638bac393aba0d0c1692a1636155fae
chinnadurai-su <chinnaduraisu23@gmail.com>
2026-04-09 00:00:12 +0530

    adding form and ng-content

--- src/app/dashboard/support-ticket/support-ticket.ts
+++ src/app/dashboard/support-ticket/support-ticket.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NewTicket } from './new-ticket/new-ticket';
 
 @Component({
   selector: 'app-support-ticket',
-  imports: [],
+  imports: [NewTicket],
   templateUrl: './support-ticket.html',
   styleUrl: './support-ticket.css',
 })
