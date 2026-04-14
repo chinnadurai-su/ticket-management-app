66522cfdf091192852f4dfac301f5141f3b46a18
chinnadurai-su <chinnaduraisu23@gmail.com>
2026-04-15 00:48:00 +0530

    host and lifeccle hook changes

--- src/app/dashboard/server-status/server-status.ts
+++ src/app/dashboard/server-status/server-status.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,6 +6,37 @@ import { Component, EventEmitter, Output } from '@angular/core';
   templateUrl: './server-status.html',
   styleUrl: './server-status.css',
 })
-export class ServerStatus {
-  currentStatus = 'online';
+// OnDestroy
+export class ServerStatus implements OnInit {
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('online');
+  private intervalData: ReturnType<typeof setInterval> | null = null;
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {}
+
+  stimulateStatusChange() {
+    if (this.intervalData) {
+      clearInterval(this.intervalData);
+    }
+    const statuses: ('online' | 'offline' | 'unknown')[] = ['online', 'offline', 'unknown'];
+    this.intervalData = setInterval(() => {
+      const randomStatus = statuses[Math.floor(Math.random() * statuses.length)];
+      this.currentStatus.set(randomStatus);
+      console.log(`Server status changed to: ${randomStatus}`);
+    }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(this.intervalData!);
+    });
+  }
+
+  ngOnInit() {
+    this.stimulateStatusChange();
+  }
+
+  // ngOnDestroy() {
+  //   if (this.intervalData) {
+  //     clearInterval(this.intervalData);
+  //   }
+  // }
 }
